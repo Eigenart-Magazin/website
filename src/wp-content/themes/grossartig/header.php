c982ae0440c7cbcfc78b2ff0dfa9cc063c6f8267
Nawarian <nickolas@phpsp.org.br>
2020-04-25 19:41:00 +0200

    feat(heute) add basic style to heute and fonts

--- src/wp-content/themes/grossartig/header.php
+++ src/wp-content/themes/grossartig/header.php
@@ -1,8 +1,66 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title><?php wp_title(''); ?> | <?php bloginfo('name'); ?></title>
+  <style type="text/css">
+    @font-face {
+      font-family: 'Apercu';
+      font-weight: 200;
+      font-style: normal;
+      src: url('<?php echo get_theme_file_uri('/assets/fonts/Apercu\ Light.otf'); ?>');
+    }
+
+    @font-face {
+      font-family: 'Apercu';
+      font-weight: 200;
+      font-style: italic;
+      src: url('<?php echo get_theme_file_uri('/assets/fonts/Apercu\ Light\ Italic.otf'); ?>');
+    }
+
+    @font-face {
+      font-family: 'Apercu';
+      font-weight: 500;
+      font-style: normal;
+      src: url('<?php echo get_theme_file_uri('/assets/fonts/Apercu\ Regular.otf'); ?>');
+    }
+
+    @font-face {
+      font-family: 'Apercu';
+      font-weight: 500;
+      font-style: italic;
+      src: url('<?php echo get_theme_file_uri('/assets/fonts/Apercu\ Italic.otf'); ?>');
+    }
+
+    @font-face {
+      font-family: 'Apercu';
+      font-weight: 600;
+      font-style: normal;
+      src: url('<?php echo get_theme_file_uri('/assets/fonts/Apercu\ Medium.otf'); ?>');
+    }
+
+    @font-face {
+      font-family: 'Apercu';
+      font-weight: 600;
+      font-style: italic;
+      src: url('<?php echo get_theme_file_uri('/assets/fonts/Apercu\ Medium\ Italic.otf'); ?>');
+    }
+
+    @font-face {
+      font-family: 'Apercu';
+      font-weight: 900;
+      font-style: normal;
+      src: url('<?php echo get_theme_file_uri('/assets/fonts/Apercu\ Bold.otf'); ?>');
+    }
+
+    @font-face {
+      font-family: 'Apercu';
+      font-weight: 900;
+      font-style: italic;
+      src: url('<?php echo get_theme_file_uri('/assets/fonts/Apercu\ Bold\ Italic.otf'); ?>');
+    }
+  </style>
   <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
+
+  <title><?php wp_title(''); ?> | <?php bloginfo('name'); ?></title>
 </head>
 <body>
 <header class="header <?php echo 'header--type-' . get_post_type(); ?>">
--- src/wp-content/themes/grossartig/templates/category/heute.php
+++ src/wp-content/themes/grossartig/templates/category/heute.php
@@ -1,13 +1,14 @@
 <article class="article article--heute">
-    <span class="article__author">
-        <?php echo get_custom_field_or_alert('article_author', 'red'); ?>
-    </span>
-
     <h1 class="article__title"><?php the_title() ?></h1>
 
     <?php if(has_excerpt()): ?>
-        <?php the_excerpt(); ?>
+        <div class="article__excerpt"><?php the_excerpt(); ?></div>
     <?php endif; ?>
+
+    <span class="article__meta">
+        05.04.2020 / NIKA GRIGORIAN / Redaktionsleitung / Art in Context
+        <!--<?php echo get_custom_field_or_alert('article_author', 'red'); ?>-->
+    </span>
 
     <?php echo get_post()->post_content; ?>
 </article>
