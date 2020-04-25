bb5506ea164c4fff3603eaf281ff38cb8c72a5a7
Nawarian <nickolas@phpsp.org.br>
2020-04-25 18:23:58 +0200

    feat(header) add sticky header

--- src/wp-content/themes/grossartig/header.php
+++ src/wp-content/themes/grossartig/header.php
@@ -5,7 +5,7 @@
   <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
 </head>
 <body>
-<header class="header">
+<header class="header <?php echo 'header--type-' . get_post_type(); ?>">
   <img
     src="<?php echo get_theme_file_uri('/assets/images/logo.png'); ?>"
     alt="<?php bloginfo('name'); ?>"
