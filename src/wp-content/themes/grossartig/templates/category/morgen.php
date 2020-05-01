a5120005c846293473b747aa9658fcd90082c187
Nawarian <nickolas@phpsp.org.br>
2020-05-01 19:11:47 +0200

    refactor(category) create category-specific scss file

--- src/wp-content/themes/grossartig/templates/category/morgen.php
+++ src/wp-content/themes/grossartig/templates/category/morgen.php
@@ -1,7 +1,6 @@
 <?php
 
 use function GrossArtig\get_top_category;
-
 $parent_category = get_top_category();
 
 $posts = query_posts([
@@ -12,43 +11,46 @@ $posts = query_posts([
 ]);
 
 ?>
-    <article>
-        <p><?php echo $parent_category->description; ?></p>
-        <hr>
-        <h1><?php echo $parent_category->name; ?></h1>
-        <ul>
-            <?php foreach ($posts as $post): ?>
-                <li>
-                    <?php echo $post->post_title; ?>
-                    <a href="<?php echo get_permalink($post); ?>">
-                        <!-- img -->
-                        <?php echo get_the_post_thumbnail($post) ?>
+  <article class="category">
+    <p class="category__description">
+      <?php echo $parent_category->description; ?>
+    </p>
+    <h1><?php echo $parent_category->name; ?></h1>
+    <ul class="articles-list">
+      <?php foreach ($posts as $post): ?>
+        <hr class="articles-list__divider">
+        <li class="articles-list__item">
+          <a href="<?php echo get_permalink($post); ?>">
+            <!-- img -->
+            <?php echo get_the_post_thumbnail($post) ?>
 
-                        <!-- pill -->
-                        <?php foreach (get_the_tags($post) as $tag): ?>
-                            <span><?php echo $tag->name; ?></span>
-                        <?php endforeach; ?>
+            <!-- pill -->
+            <?php foreach (get_the_tags($post) as $tag): ?>
+              <div class="articles-list__item-tags">
+                <span class="pill"><?php echo $tag->name; ?></span>
+              </div>
+            <?php endforeach; ?>
 
-                        <?php
-                        $custom = get_post_custom($post->ID) ?? [];
-                        $meta = $custom['article_author'] ?? [];
-                        $date = new DateTimeImmutable($post->post_date);
-                        $meta = array_merge([$date->format('d.m.Y')], $meta);
-                        ?>
-                        <!-- meta -->
-                        <span><?php echo implode(' / ', $meta); ?></span>
-
-                        <!-- title -->
-                        <h2><?php echo $post->post_title; ?></h2>
-                        <!-- paragraph -->
-                        <p><?php echo $post->post_excerpt; ?></p>
-                    </a>
-                </li>
             <?php
-            endforeach;
+            $custom = get_post_custom($post->ID) ?? [];
+            $meta = $custom['article_author'] ?? [];
+            $date = new DateTimeImmutable($post->post_date);
+            $meta = array_merge([$date->format('d.m.Y')], $meta);
             ?>
-        </ul>
-    </article>
+            <!-- meta -->
+            <span class="articles-list__item-meta"><?php echo implode(' / ', $meta); ?></span>
+
+            <!-- title -->
+            <h2><?php echo $post->post_title; ?></h2>
+            <!-- paragraph -->
+            <p><?php echo $post->post_excerpt; ?></p>
+          </a>
+        </li>
+      <?php
+      endforeach;
+      ?>
+    </ul>
+  </article>
 
 <?php
 wp_reset_query();
