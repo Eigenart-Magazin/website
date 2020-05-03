83170a27aa5709a206d438bdc8e2267c784e1bee
Nawarian <nickolas@phpsp.org.br>
2020-05-03 23:58:17 +0200

    feat(footer) add desktop support to website's footer

--- src/wp-content/themes/grossartig/footer.php
+++ src/wp-content/themes/grossartig/footer.php
@@ -1,14 +1,16 @@
 </main>
 <footer class="footer">
-    <ul>
-    <?php foreach (wp_get_nav_menu_items('footer') ?: [] as $menu_item): ?>
-        <li>
-            <a href="<?php echo $menu_item->url ?: '#'; ?>"><?php echo $menu_item->title; ?></a>
-        </li>
-    <?php endforeach; ?>
-    </ul>
+  <ul>
+  <?php foreach (wp_get_nav_menu_items('footer') ?: [] as $menu_item): ?>
+    <li>
+      <a class="pill pill--inverted" href="<?php echo $menu_item->url ?: '#'; ?>">
+        <?php echo $menu_item->title; ?>
+      </a>
+    </li>
+  <?php endforeach; ?>
+  </ul>
 
-    <span>© Copyright 2020 - <?php bloginfo('name'); ?></span>
+  <span>© Copyright 2020 - <?php bloginfo('name'); ?></span>
 </footer>
 </body>
 </html>
