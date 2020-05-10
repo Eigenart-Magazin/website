5b5fadb2b41a923c4758e659529ee3f60224545c
Nawarian <nickolas@phpsp.org.br>
2020-05-11 01:59:24 +0200

    feat(home) add home buttons

--- src/wp-content/themes/grossartig/HomePage.php
+++ src/wp-content/themes/grossartig/HomePage.php
@@ -5,31 +5,63 @@ get_header();
 
 $menu_items = wp_get_nav_menu_items('home-main-menu');
 ?>
+<script type="text/javascript">
+  function onMouseOverHomeButton(img) {
+    img.src = img.getAttribute('data-src-active');
+  }
+
+  function onMouseLeaveHomeButton(img) {
+    img.src = img.getAttribute('data-src-idle');
+  }
+</script>
 <div class="home">
   <?php echo get_post()->post_content; ?>
   <ul class="category-list">
-    <?php foreach ($menu_items as $item): ?>
+    <?php foreach ($menu_items as $pos => $item): ?>
       <li class="category-list__item">
+        <?php if ($pos % 2 === 0): ?>
+        <div class="category-list__arrow">
+<!--          <img src="--><?php //echo get_theme_file_uri('/assets/images/arrow-home-page.png'); ?><!--" alt="" />-->
+<!--          <img class="small" src="--><?php //echo get_theme_file_uri('/assets/images/arrow-home-page-small.png'); ?><!--" alt="" />-->
+        </div>
+        <?php endif; ?>
         <a href="<?php echo $item->url; ?>">
           <?php
           $img = '';
+          $imgActive = '';
           switch (strtolower($item->title)) {
               case 'morgen':
                   $img =  get_theme_file_uri('/assets/images/home-morgen.png');
+                  $imgActive =  get_theme_file_uri('/assets/images/home-morgen-active.png');
                   break;
               case 'heute':
                   $img =  get_theme_file_uri('/assets/images/home-heute.png');
+                  $imgActive =  get_theme_file_uri('/assets/images/home-heute-active.png');
                   break;
               case 'gestern':
                   $img =  get_theme_file_uri('/assets/images/home-gestern.png');
+                  $imgActive =  get_theme_file_uri('/assets/images/home-gestern-active.png');
                   break;
               case 'about':
                   $img =  get_theme_file_uri('/assets/images/home-about.png');
+                  $imgActive =  get_theme_file_uri('/assets/images/home-about-active.png');
                   break;
           }
           ?>
-          <img src="<?php echo $img; ?>" alt="<?php echo $item->title; ?>" />
+          <img
+            onmouseover="onMouseOverHomeButton(this)"
+            onmouseleave="onMouseLeaveHomeButton(this)"
+            data-src-idle="<?php echo $img; ?>"
+            data-src-active="<?php echo $imgActive; ?>"
+            src="<?php echo $img; ?>" alt="<?php echo $item->title; ?>"
+          />
         </a>
+        <?php if ($pos % 2 === 1): ?>
+        <div class="category-list__arrow category-list__arrow--right">
+<!--          <img src="--><?php //echo get_theme_file_uri('/assets/images/arrow-home-page.png'); ?><!--" alt="" />-->
+<!--          <img class="small" src="--><?php //echo get_theme_file_uri('/assets/images/arrow-home-page-small-right.png'); ?><!--" alt="" />-->
+        </div>
+        <?php endif; ?>
       </li>
     <? endforeach; ?>
   </ul>
