3fe4809beecf4e4c70b762ecf36a9a1a315e8870
Nawarian <nickolas@phpsp.org.br>
2020-06-09 21:30:26 +0200

    feat(Morgen) add bubbles to element items on Mogen category page #21

--- src/wp-content/themes/grossartig/category.php
+++ src/wp-content/themes/grossartig/category.php
@@ -57,44 +57,47 @@ $is_morgen = 'morgen' === strtolower($parent_category->name);
                 <!-- img -->
                 <?php
                 echo get_the_post_thumbnail($post, 'post-thumbnail', [
-                    'class' => 'articles-list__cover'
+                  'class' => 'articles-list__cover'
                 ]);
                 ?>
 
-                <!-- pill -->
-                <?php foreach (get_the_tags($post) as $tag): ?>
-    <!--              <div class="articles-list__item-tags">-->
-    <!--                <span class="pill">--><?php //echo $tag->name; ?><!--</span>-->
-    <!--              </div>-->
-                <?php endforeach; ?>
-
-                <?php
-                $custom = get_post_custom($post->ID) ?? [];
-                $meta = $custom['article_author'] ?? [];
-                $date = new DateTimeImmutable($post->post_date);
-                $meta = array_merge([$date->format('d.m.Y')], $meta);
-                ?>
-                <!-- meta -->
-                <span class="articles-list__item-meta"><?php echo implode(' / ', $meta); ?></span>
-
-                <!-- title -->
-                <h2><?php echo $post->post_title; ?></h2>
-                <!-- paragraph -->
-                <p>
-                  <?php echo $post->post_excerpt; ?>
-                  <?php if ($is_morgen): ?>
-                  <img
-                    class="articles-list__read-more"
-                    src="<?php echo get_theme_file_uri('/assets/images/arrow-read-more.png'); ?>"
-                    alt=""
-                  />
-                  <img
-                    class="articles-list__read-more articles-list__read-more--inverted"
-                    src="<?php echo get_theme_file_uri('/assets/images/short-white-arrow-right.png'); ?>"
-                    alt=""
-                  />
-                  <?php endif; ?>
-                </p>
+                <div class="bubble-container">
+                  <!-- pill -->
+                  <?php foreach (get_the_tags($post) as $tag): ?>
+                    <div class="articles-list__item-tags">
+                      <span class="pill"><?php echo $tag->name; ?></span>
+                    </div>
+                  <?php endforeach; ?>
+                </div>
+                <div class="container">
+                  <?php
+                  $custom = get_post_custom($post->ID) ?? [];
+                  $meta = $custom['article_author'] ?? [];
+                  $date = new DateTimeImmutable($post->post_date);
+                  $meta = array_merge([$date->format('d.m.Y')], $meta);
+                  ?>
+                  <!-- meta -->
+                  <span class="articles-list__item-meta"><?php echo implode(' / ', $meta); ?></span>
+    
+                  <!-- title -->
+                  <h2><?php echo $post->post_title; ?></h2>
+                  <!-- paragraph -->
+                  <p>
+                    <?php echo $post->post_excerpt; ?>
+                    <?php if ($is_morgen): ?>
+                    <img
+                      class="articles-list__read-more"
+                      src="<?php echo get_theme_file_uri('/assets/images/arrow-read-more.png'); ?>"
+                      alt=""
+                    />
+                    <img
+                      class="articles-list__read-more articles-list__read-more--inverted"
+                      src="<?php echo get_theme_file_uri('/assets/images/short-white-arrow-right.png'); ?>"
+                      alt=""
+                    />
+                    <?php endif; ?>
+                  </p>
+                </div>
               </a>
             </li>
           <?php
