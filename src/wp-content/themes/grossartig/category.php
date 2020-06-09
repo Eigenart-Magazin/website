b965e765c4427c221b28fa35bcfea814b2a823b5
Nawarian <nickolas@phpsp.org.br>
2020-06-09 20:00:13 +0200

    refactor(Category) split scss files between Morgen and Heute

--- src/wp-content/themes/grossartig/category.php
+++ src/wp-content/themes/grossartig/category.php
@@ -18,71 +18,73 @@ $category = get_the_category()[0];
 $is_heute = 'heute' === strtolower($parent_category->name);
 ?>
 
-<div class="category <?php echo $is_heute ? 'category--heute' : '' ?>">
-  <aside class="category__aside"></aside>
-  <article class="category__main">
-    <div class="category__description">
-      <p>
-        <?php echo $parent_category->description; ?>
-      </p>
-    </div>
-    <?php if (true === $is_heute): ?>
-    <div class="category__description category__description--contrast-background">
-      <div class="category__description--open-call">
-        <h1>
-          <img
-            class="category__open-call-arrow"
-            src="<?php echo get_theme_file_uri('/assets/images/arrow-right.png'); ?>"
-            alt=""
-          />
-          <span><?php echo $category->name; ?></span>
-        </h1>
-        <p><?php echo $category->description; ?></p>
-      </div>
-    </div>
-    <?php endif; ?>
-    <ul class="articles-list">
-      <?php foreach ($posts as $post): ?>
-        <li class="articles-list__item">
-          <a href="<?php echo get_permalink($post); ?>">
-            <!-- img -->
-            <?php echo get_the_post_thumbnail($post) ?>
+<div class="<?php echo $is_heute ? 'category-heute' : 'category-morgen' ?>">
+  <div class="category">
+      <aside class="category__aside"></aside>
+      <article class="category__main">
+        <div class="category__description">
+          <p>
+            <?php echo $parent_category->description; ?>
+          </p>
+        </div>
+        <?php if (true === $is_heute): ?>
+        <div class="category__description category__description--contrast-background">
+          <div class="category__description--open-call">
+            <h1>
+              <img
+                class="category__open-call-arrow"
+                src="<?php echo get_theme_file_uri('/assets/images/arrow-right.png'); ?>"
+                alt=""
+              />
+              <span><?php echo $category->name; ?></span>
+            </h1>
+            <p><?php echo $category->description; ?></p>
+          </div>
+        </div>
+        <?php endif; ?>
+        <ul class="articles-list">
+          <?php foreach ($posts as $post): ?>
+            <li class="articles-list__item">
+              <a href="<?php echo get_permalink($post); ?>">
+                <!-- img -->
+                <?php echo get_the_post_thumbnail($post) ?>
 
-            <!-- pill -->
-            <?php foreach (get_the_tags($post) as $tag): ?>
-<!--              <div class="articles-list__item-tags">-->
-<!--                <span class="pill">--><?php //echo $tag->name; ?><!--</span>-->
-<!--              </div>-->
-            <?php endforeach; ?>
+                <!-- pill -->
+                <?php foreach (get_the_tags($post) as $tag): ?>
+    <!--              <div class="articles-list__item-tags">-->
+    <!--                <span class="pill">--><?php //echo $tag->name; ?><!--</span>-->
+    <!--              </div>-->
+                <?php endforeach; ?>
 
-            <?php
-            $custom = get_post_custom($post->ID) ?? [];
-            $meta = $custom['article_author'] ?? [];
-            $date = new DateTimeImmutable($post->post_date);
-            $meta = array_merge([$date->format('d.m.Y')], $meta);
-            ?>
-            <!-- meta -->
-            <span class="articles-list__item-meta"><?php echo implode(' / ', $meta); ?></span>
+                <?php
+                $custom = get_post_custom($post->ID) ?? [];
+                $meta = $custom['article_author'] ?? [];
+                $date = new DateTimeImmutable($post->post_date);
+                $meta = array_merge([$date->format('d.m.Y')], $meta);
+                ?>
+                <!-- meta -->
+                <span class="articles-list__item-meta"><?php echo implode(' / ', $meta); ?></span>
 
-            <!-- title -->
-            <h2><?php echo $post->post_title; ?></h2>
-            <!-- paragraph -->
-            <p>
-              <?php echo $post->post_excerpt; ?>
-            </p>
-          </a>
-        </li>
-      <?php
-      endforeach;
-      ?>
-    </ul>
-    <a href="#" class="category__scroll-top">
-        <img
-          src="<?php echo get_theme_file_uri('/assets/images/arrow-top.png'); ?>"
-          alt="Scroll to Top"
-        />
-    </a>
-  </article>
+                <!-- title -->
+                <h2><?php echo $post->post_title; ?></h2>
+                <!-- paragraph -->
+                <p>
+                  <?php echo $post->post_excerpt; ?>
+                </p>
+              </a>
+            </li>
+          <?php
+          endforeach;
+          ?>
+        </ul>
+        <a href="#" class="category__scroll-top">
+            <img
+              src="<?php echo get_theme_file_uri('/assets/images/arrow-top.png'); ?>"
+              alt="Scroll to Top"
+            />
+        </a>
+      </article>
+  </div>
 </div>
 <?php
 wp_reset_query();
