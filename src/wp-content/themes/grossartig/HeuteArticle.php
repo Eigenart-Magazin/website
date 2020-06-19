4d063c3964ff86b92ef5955c0ed4d1153acc8fef
Nawarian <nickolas@phpsp.org.br>
2020-06-19 16:14:19 +0200

    refactor(Article) adjust post and category fetching

--- src/wp-content/themes/grossartig/HeuteArticle.php
+++ src/wp-content/themes/grossartig/HeuteArticle.php
@@ -4,27 +4,20 @@
  * Template Post Type: post
  */
 
-use function GrossArtig\{
-    get_custom_field_or_alert,
-    get_top_category
-};
+use function GrossArtig\{get_custom_field_or_alert};
 
 get_header();
-
 the_post();
 
-$category = get_top_category();
-$post = get_post();
-
 ?>
-  <article class="article article--heute">
-    <div class="article__header">
-      <button class="article__back-button" onclick="window.history.go(-1);">
-        <img src="<?php echo get_theme_file_uri('/assets/images/short-arrow-left.png'); ?>" alt="Go Back" />
-      </button>
-      <h1 class="article__title"><?php the_title() ?></h1>
+<article class="article article--heute">
+  <div class="article__header">
+    <button class="article__back-button" onclick="window.history.go(-1);">
+      <img src="<?php echo get_theme_file_uri('/assets/images/short-arrow-left.png'); ?>" alt="Go Back" />
+    </button>
+    <h1 class="article__title"><?php the_title() ?></h1>
 
-      <span class="article__meta">
+    <span class="article__meta">
         <?php
         $date = get_post_datetime() ?: new DateTimeImmutable();
         $fields = array_merge(
@@ -34,66 +27,63 @@ $post = get_post();
 
         echo implode(' / ', $fields);
         ?>
-      </span>
+    </span>
 
-      <?php if(has_excerpt()): ?>
-        <div class="article__excerpt"><?php the_excerpt(); ?></div>
-      <?php endif; ?>
-    </div>
+    <?php if(has_excerpt()): ?>
+    <div class="article__excerpt"><?php the_excerpt(); ?></div>
+    <?php endif; ?>
+  </div>
 
-    <div class="article__content">
-      <?php the_content(); ?>
-    </div>
-  </article>
-
+  <div class="article__content"><?php the_content(); ?></div>
+</article>
 <?php
-
+$category = get_the_category()[0];
 $recommended_articles = query_posts([
   'cat' => $category->term_id,
-  'post__not_in' => [$post->ID],
+  'post__not_in' => [get_post_field('ID')],
   'posts_per_page' => 5,
 ]) ?: [];
 
 wp_reset_query();
 ?>
-  <?php if (count($recommended_articles) > 0): ?>
-  <aside class="article__recommendations article__recommendations--heute">
-    <header class="article__recommendations-header">
-      <div class="arrow-container arrow-container--thin arrow-container--left">
-        <div class="arrow-container__arrow arrow-container__arrow--left">
-          <i class="arrow-container__arrow-head"></i>
-        </div>
-      </div>
-      <h2>mehr & much more</h2>
-    </header>
-    <ul class="article__recommendations-list">
-      <?php foreach ($recommended_articles as $recommended): ?>
-        <li class="article__recommended-list-item">
-          <a href="<?php echo get_permalink($recommended); ?>">
-            <?php echo get_the_post_thumbnail($recommended) ?>
-            <?php
+<?php if (count($recommended_articles) > 0): ?>
+<aside class="article__recommendations article__recommendations--heute">
+  <header class="article__recommendations-header">
+    <div class="arrow-container arrow-container--thin arrow-container--left">
+    <div class="arrow-container__arrow arrow-container__arrow--left">
+      <i class="arrow-container__arrow-head"></i>
+    </div>
+    </div>
+    <h2>mehr & much more</h2>
+  </header>
+  <ul class="article__recommendations-list">
+    <?php foreach ($recommended_articles as $recommended): ?>
+    <li class="article__recommended-list-item">
+      <a href="<?php echo get_permalink($recommended); ?>">
+        <?php echo get_the_post_thumbnail($recommended) ?>
+        <?php
             $custom = get_post_custom($recommended->ID) ?? [];
             $meta = $custom['article_author'] ?? [];
             $date = new DateTimeImmutable($recommended->post_date);
             $meta = array_merge([$date->format('d.m.Y')], $meta);
-            ?>
-            <span><?php echo implode(' / ', $meta); ?></span>
+        ?>
+        <span><?php echo implode(' / ', $meta); ?></span>
 
-            <h3><?php echo $recommended->post_title; ?></h3>
-            <p><?php echo $recommended->post_excerpt; ?></p>
-          </a>
-        </li>
-      <?php endforeach; ?>
-    </ul>
-  </aside>
-  <?php endif; ?>
+        <h3><?php echo $recommended->post_title; ?></h3>
+        <p><?php echo $recommended->post_excerpt; ?></p>
+      </a>
+    </li>
+    <?php endforeach; ?>
+  </ul>
+</aside>
+<?php endif; ?>
 
-  <a href="#" class="category__scroll-top">
-    <img
-      src="<?php echo get_theme_file_uri('/assets/images/arrow-top.png'); ?>"
-      alt="Scroll to Top"
-    />
-  </a>
+<a href="#" class="category__scroll-top">
+  <img
+    src="<?php echo get_theme_file_uri('/assets/images/arrow-top.png'); ?>"
+    alt="Scroll to Top"
+  />
+</a>
 
 <?php
 get_footer();
--- src/wp-content/themes/grossartig/MorgenArticle.php
+++ src/wp-content/themes/grossartig/MorgenArticle.php
@@ -5,94 +5,86 @@
  * Template Post Type: post
  */
 
-use function GrossArtig\{
-  get_custom_field_or_alert,
-  get_top_category
-};
+use function GrossArtig\{get_custom_field_or_alert};
 
 get_header();
 the_post();
 
-$category = get_top_category();
-$post = get_post();
-
 ?>
-  <!-- pill -->
-  <?php foreach (get_the_tags($post) as $tag): ?>
-    <div class="article-tags">
-      <span class="pill"><?php echo $tag->name; ?></span>
-    </div>
-  <?php endforeach; ?>
-  <article class="article article--morgen">
-    <aside class="article__side"></aside>
-    <div class="article__main">
-      <div class="article__header">
-        <h1 class="article__title"><?php the_title() ?></h1>
+<!-- pill -->
+<?php foreach (get_the_tags($post) as $tag): ?>
+<div class="article-tags">
+  <span class="pill"><?php echo $tag->name; ?></span>
+</div>
+<?php endforeach; ?>
+<article class="article article--morgen">
+  <aside class="article__side"></aside>
+  <div class="article__main">
+    <div class="article__header">
+      <h1 class="article__title"><?php the_title() ?></h1>
 
-        <?php if(has_excerpt()): ?>
-          <div class="article__excerpt"><?php the_excerpt(); ?></div>
-        <?php endif; ?>
+      <?php if(has_excerpt()): ?><div class="article__excerpt"><?php the_excerpt(); ?></div><?php endif; ?>
 
-        <span class="article__meta">
+      <span class="article__meta">
           <?php
-          $date = get_post_datetime() ?: new DateTimeImmutable();
-          $fields = array_merge(
-            [$date->format('d.m.Y')],
-            get_custom_field_or_alert('article_author', 'red')
-          );
+              $date = get_post_datetime() ?: new DateTimeImmutable();
+              $fields = array_merge(
+                  [$date->format('d.m.Y')],
+                  get_custom_field_or_alert('article_author', 'red')
+              );
 
-          echo implode(' / ', $fields);
+              echo implode(' / ', $fields);
           ?>
-        </span>
-      </div>
-
-      <div class="article__content">
-        <?php the_content(); ?>
-      </div>
+      </span>
     </div>
-  </article>
+
+    <div class="article__content"><?php the_content(); ?></div>
+  </div>
+</article>
 
 <?php
 
+$category = get_the_category()[0];
+
 $recommended_articles = query_posts([
   'cat' => $category->term_id,
-  'post__not_in' => [$post->ID],
+  'post__not_in' => [get_post_field('ID')],
   'posts_per_page' => 5,
 ]) ?: [];
 
 wp_reset_query();
 ?>
-  <?php if (count($recommended_articles) > 0): ?>
-  <aside class="article__recommendations article__recommendations--morgen">
-    <header class="article__recommendations-header">
-      <div class="arrow-container arrow-container--thin arrow-container--left">
-        <div class="arrow-container__arrow arrow-container__arrow--left">
-          <i class="arrow-container__arrow-head"></i>
-        </div>
+<?php if (count($recommended_articles) > 0): ?>
+<aside class="article__recommendations article__recommendations--morgen">
+  <header class="article__recommendations-header">
+    <div class="arrow-container arrow-container--thin arrow-container--left">
+      <div class="arrow-container__arrow arrow-container__arrow--left">
+        <i class="arrow-container__arrow-head"></i>
       </div>
-      <h2>mehr & much more</h2>
-    </header>
-    <ul class="article__recommendations-list">
-      <?php foreach ($recommended_articles as $recommended): ?>
-        <li class="article__recommended-list-item">
-          <a href="<?php echo get_permalink($recommended); ?>">
-            <?php echo get_the_post_thumbnail($recommended) ?>
-            <?php
+    </div>
+    <h2>mehr & much more</h2>
+  </header>
+  <ul class="article__recommendations-list">
+    <?php foreach ($recommended_articles as $recommended): ?>
+    <li class="article__recommended-list-item">
+      <a href="<?php echo get_permalink($recommended); ?>">
+        <?php echo get_the_post_thumbnail($recommended) ?>
+        <?php
             $custom = get_post_custom($recommended->ID) ?? [];
             $meta = $custom['article_author'] ?? [];
             $date = new DateTimeImmutable($recommended->post_date);
             $meta = array_merge([$date->format('d.m.Y')], $meta);
-            ?>
-            <span><?php echo implode(' / ', $meta); ?></span>
+        ?>
+        <span><?php echo implode(' / ', $meta); ?></span>
 
-            <h3><?php echo $recommended->post_title; ?></h3>
-            <p><?php echo $recommended->post_excerpt; ?></p>
-          </a>
-        </li>
-      <?php endforeach; ?>
-    </ul>
-  </aside>
-  <?php endif; ?>
+        <h3><?php echo $recommended->post_title; ?></h3>
+        <p><?php echo $recommended->post_excerpt; ?></p>
+      </a>
+    </li>
+    <?php endforeach; ?>
+  </ul>
+</aside>
+<?php endif; ?>
 
 <?php
 get_footer();
