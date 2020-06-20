1204a13d07324039d33956a9c6ad6c8872781312
Nawarian <nickolas@phpsp.org.br>
2020-06-20 19:28:38 +0200

    feat(gestern) add additional data for gestern pages

--- src/wp-content/themes/grossartig/category-gestern.php
+++ src/wp-content/themes/grossartig/category-gestern.php
@@ -43,7 +43,13 @@ get_header('category');
       <ul>
         <?php while(have_posts()): the_post(); ?>
         <?php
-        $year = the_date('Y', '', '', false);
+        $year = get_post_datetime();
+        if (false === $year) {
+            $year = 'unknown';
+        } else {
+            $year = $year->format('Y');
+        }
+
         ?>
         <li class="gestern-print__post">
           <div class="gestern-print__post-head">
@@ -55,9 +61,60 @@ get_header('category');
           <?php
               echo get_the_post_thumbnail($post, 'post-thumbnail', ['class' => 'gestern-print__post-cover']);
           ?>
+          <!-- Paragraph -->
+          <?php the_excerpt(); ?>
+
+          <!-- Attachment download links -->
           <?php
-              the_excerpt();
+          $attached_files = get_attached_media('*');
+
+          $attachment = false;
+          if (count($attached_files) > 0) {
+              $attachment = array_shift($attached_files);
+          }
           ?>
+          <?php if ($attachment): ?>
+          <?php
+          $attachment_link = wp_get_attachment_url($attachment->ID);
+          $file_size = filesize(get_attached_file($attachment->ID));
+          ?>
+          <span class="gestern-print__download">
+            <a href="<?php echo $attachment_link; ?>">
+              download file: <?php echo round($file_size / 1024 / 1024, 2); ?> Mb
+            </a>
+          </span>
+          <?php endif; ?>
+
+          <!-- Meta items -->
+          <?php
+          $editor = get_post_field('editor');
+          $layout = get_post_field('layout');
+          $copies = get_post_field('copies');
+          ?>
+          <?php if ($editor || $layout || $copies): ?>
+          <div class="gestern-print__meta">
+            <?php if ($editor): ?>
+            <div class="gestern-print__meta-item">
+              <span class="gestern-print__meta-item-title">Redaktionsleitung</span>
+              <div><?php echo $editor; ?></div>
+            </div>
+            <?php endif; ?>
+
+            <?php if ($layout): ?>
+              <div class="gestern-print__meta-item">
+                <span class="gestern-print__meta-item-title">Gestaltung</span>
+                <div><?php echo $layout; ?></div>
+              </div>
+            <?php endif; ?>
+
+            <?php if ($copies): ?>
+            <div class="gestern-print__meta-item">
+              <span class="gestern-print__meta-item-title">Auflage</span>
+              <div><?php echo $copies; ?></div>
+            </div>
+            <?php endif; ?>
+          </div>
+          <?php endif; ?>
         </li>
         <?php endwhile; ?>
       </ul>
