0673ccfe62b3a7d0323acc0786590048cb08b00b
Nawarian <nickolas@phpsp.org.br>
2020-08-02 22:49:29 +0200

    fix(feed) adjust wrong image and audio link

--- src/wp-content/themes/grossartig/functions.php
+++ src/wp-content/themes/grossartig/functions.php
@@ -68,6 +68,10 @@ function grossartig_unsafe_image_url(string $imageUrl) {
 /**
  * Podcast plugin
  */
+add_filter('option_permalink_structure', function () {
+    return false;
+});
+add_filter('ssp_feed_image', 'GrossArtig\grossartig_unsafe_image_url');
 add_filter('ssp_feed_item_image', 'GrossArtig\grossartig_unsafe_image_url');
 
 /**
